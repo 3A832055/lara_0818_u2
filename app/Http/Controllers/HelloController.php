1d50819b897aa77983e55487590761971c01e46a
蔡羽宣 <3a832055@gm.student.ncut.edu.tw>
2021-11-02 22:41:28 +0800

    設定Route

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\HelloController;
 
 class HelloController extends Controller
 {
     //
+    Route::get('hello/{name?}',[HelloController::class,'index'])->name('hello.index');
 }
